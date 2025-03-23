7041cd021e35c697b09d074ebc56a3e9e58fa9a7
vaiserk <ootaviooffreitass@gmail.com>
2025-03-23 20:52:07 -0300

    lista de adjacencia

--- digistra em c/algo.c
+++ digistra em c/algo.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int numero;
+   
     
-    printf("Digite um numero: ");
-    scanf("%d", &numero);
-    
-    if (numero % 2 == 0) {
-        printf("O numero %d e par.\n", numero);
-    } else {
-        printf("O numero %d é impar.\n", numero);
-        printf("O numero %d é impar.\n", numero);
-        printf("ou");
-                printf("O numero %d é impar.\n", numero);
-
-    }
     
     return 0;
 }
